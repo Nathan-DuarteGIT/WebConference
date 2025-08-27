5ca6610fe867edb22c91a9f6215b2b3f80b94246
Nathan-DuarteGIT <duartenathan60@gmail.com>
2025-08-27 19:09:09 +0100

    git commit -m "criação dos ficheiros iniciais da app web"

---
+++ js/webconf.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+    const btnRegister = document.getElementById("btnRegister")
+    btnRegister.addEventListener("click", function(){
+        //abertura da janela modal
+        swal({
+            title: "Inscrição na WebConference",
+            html:
+            '<input id="txtName" class="swal2-input" placeholder="name">' +
+            '<input id="txtEmail" class="swal2-input" placeholder="e-mail">',
+            showCancelButton: true,
+            confirmButtonText: "Inscrever",
+            cancelButtonText: "Cancelar",
+            showLoaderOnConfirm: true,
+            preConfirm: () =>{
+                const name = document.getElementById('txtName').value
+                const email = document.getElementById('txtEmail').value
+                const url_base = "https://fcawebbook.herokuapp.com"
+                return fetch(`${url_base}/conferences/1/participants/${email}`,{
+                        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                        method: "POST",
+                        body: `nomeparticipant=${name}`
+                    })
+                    .then(response =>{
+                        if(!response.ok) {
+                            throw new Error(response.statusText);
+                        }
+                        return response.json();
+                    })
+                    .catch(error => {
+                        swal.showValidationError(`Pedido Falhou: ${error}`);
+                    });
+            }, allowOutsideClick: () => !swal.isLoading()
+        }).then(result => {
+            if(result.value){
+                if(!result.value.err_code) {
+                    swal({title: "Inscrição feita com sucesso!"})
+                    console.log("entrei sucesso");
+                } else {
+                    swal({title: `${result.value.err_message}`})
+                    console.log("entrei erro");
+                }
+            }
+        })
+    })
+}
